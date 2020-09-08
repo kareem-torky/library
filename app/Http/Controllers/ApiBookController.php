907493488220c0166180b8bc142db5402f9f3440
Kareem M. Fouad <kareemmfouad.ece@gmail.com>
2020-09-08 18:24:04 +0200

    books crud api

---
+++ app/Http/Controllers/ApiBookController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+use Illuminate\Support\Facades\Validator;
+
+class ApiBookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::get();
+
+        return response()->json($books);
+    }
+
+    public function show($id)
+    {
+        $book = Book::findOrFail($id);
+
+        return response()->json($book);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:100',
+            'desc' => 'required|string',
+            'img' => 'required|image|mimes:jpg,png',
+            'category_ids' => 'required',
+            'category_ids.*' => 'exists:categories,id',
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response()->json($errors);
+        }
+
+        // move 
+        $img = $request->file('img');
+        $ext = $img->getClientOriginalExtension();
+        $name = "book-". uniqid() . ".$ext";
+        $img->move( public_path('uploads/books') , $name);
+
+        $book = Book::create([
+            'title' => $request->title,
+            'desc' => $request->desc,
+            'img' => $name
+        ]);
+
+        $book->categories()->sync($request->category_ids);
+        $success = "book created successfully";
+
+        return response()->json($success);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:100',
+            'desc' => 'required|string',
+            'img' => 'required|image|mimes:jpg,png',
+            'category_ids' => 'required',
+            'category_ids.*' => 'exists:categories,id',
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response()->json($errors);
+        }
+
+        $book = Book::findOrFail($id);
+        $name = $book->img;
+
+        if($request->hasFile('img'))
+        {
+            if($name !== null) 
+            {
+                unlink( public_path('uploads/books/') . $name );
+            }
+
+            $img = $request->file('img');
+            $ext = $img->getClientOriginalExtension();
+            $name = "book-". uniqid() . ".$ext";
+            $img->move( public_path('uploads/books/') , $name);
+        }
+
+        $book->update([
+            'title' => $request->title,
+            'desc' => $request->desc,
+            'img' => $name
+        ]);
+
+        $book->categories()->sync($request->category_ids);
+
+        $success = "book updated successfully";
+
+        return response()->json($success);
+    }
+
+    public function delete($id)
+    {
+        $book = Book::findOrFail($id);
+
+        if($book->img !== null) 
+        {
+            unlink( public_path('uploads/books/') . $book->img );
+        }
+
+        $book->categories()->sync([]);
+
+        $book->delete();
+
+        $success = "book deleted successfully";
+
+        return response()->json($success);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Books API 
+Route::get('/books', 'ApiBookController@index');
+Route::get('/books/show/{id}', 'ApiBookController@show');
+Route::post('/books/store', 'ApiBookController@store');
+Route::post('/books/update/{id}', 'ApiBookController@update');
+Route::get('/books/delete/{id}', 'ApiBookController@delete');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// /books/show/{id}
